a9e4d1b2a082ca1bf6e5965a4e05f96b0ea8a139
Mateus Farias <mateus.f2001@hotmail.com>
2025-01-17 22:51:10 +0000

    feat: criar controlador para gerenciar veículos

---
+++ src/main/java/com/roadlogic/controllers/VehicleController.java
@@ -0,0 +1,36 @@
+package com.roadlogic.controllers;
+
+import com.roadlogic.models.Vehicle;
+import com.roadlogic.services.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/vehicles")
+public class VehicleController {
+
+    @Autowired
+    private VehicleService vehicleService;
+
+    @PostMapping
+    public Vehicle addVehicle(@RequestBody Vehicle vehicle) {
+        return vehicleService.saveVehicle(vehicle);
+    }
+
+    @GetMapping
+    public List<Vehicle> getAllVehicles() {
+        return vehicleService.getAllVehicles();
+    }
+
+    @PutMapping("/{id}")
+    public Vehicle updateVehicle(@PathVariable Long id, @RequestBody Vehicle vehicle) {
+        return vehicleService.updateVehicle(id, vehicle);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteVehicle(@PathVariable Long id) {
+        vehicleService.deleteVehicle(id);
+    }
+}
