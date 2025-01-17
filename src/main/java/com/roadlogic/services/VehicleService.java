b5c6ac23e727619b16023306d5fada4a9fb434fd
Mateus Farias <mateus.f2001@hotmail.com>
2025-01-17 22:46:45 +0000

    feat: adiciona VehicleService com métodos básicos de CRUD

---
+++ src/main/java/com/roadlogic/services/VehicleService.java
@@ -0,0 +1,23 @@
+package com.roadlogic.services;
+
+import com.roadlogic.models.Vehicle;
+import com.roadlogic.repositories.VehicleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class VehicleService {
+
+    @Autowired
+    private VehicleRepository vehicleRepository;
+
+    public List<Vehicle> getAllVehicles() {
+        return vehicleRepository.findAll();
+    }
+
+    public Vehicle addVehicle(Vehicle vehicle) {
+        return vehicleRepository.save(vehicle);
+    }
+}
