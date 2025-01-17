7c06919431a9f4b20d273eecaeefcda4d41c15bf
Mateus Farias <mateus.f2001@hotmail.com>
2025-01-17 23:22:36 +0000

    fix: corrige métodos e melhora integração com modelo Vehicle

--- src/main/java/com/roadlogic/services/VehicleService.java
+++ src/main/java/com/roadlogic/services/VehicleService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class VehicleService {
@@ -17,7 +18,29 @@ public class VehicleService {
         return vehicleRepository.findAll();
     }
 
-    public Vehicle addVehicle(Vehicle vehicle) {
+    public Vehicle saveVehicle(Vehicle vehicle) { 
         return vehicleRepository.save(vehicle);
+    }
+
+    public Optional<Vehicle> getVehicleById(Long id) {
+        return vehicleRepository.findById(id);
+    }
+
+    public Vehicle updateVehicle(Long id, Vehicle vehicle) {
+        return vehicleRepository.findById(id)
+                .map(existingVehicle -> {
+                    existingVehicle.setType(vehicle.getType());
+                    existingVehicle.setSpeed(vehicle.getSpeed());
+                    existingVehicle.setMaxSpeed(vehicle.getMaxSpeed());
+                    existingVehicle.setPosition(vehicle.getPosition());
+                    existingVehicle.setDirection(vehicle.getDirection());
+                    existingVehicle.setEmergency(vehicle.isEmergency());
+                    return vehicleRepository.save(existingVehicle);
+                })
+                .orElseThrow(() -> new RuntimeException("Vehicle not found with id " + id));
+    }
+
+    public void deleteVehicle(Long id) {
+        vehicleRepository.deleteById(id);
     }
 }
