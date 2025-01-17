7daaf6130729f83b3428949521c32c435f97cd53
Mateus Farias <mateus.f2001@hotmail.com>
2025-01-17 22:40:01 +0000

    refactor: ajustes na entidade Street para adicionar relações com Intersection e Vehicle

--- src/main/java/com/roadlogic/models/Street.java
+++ src/main/java/com/roadlogic/models/Street.java
@@ -1,17 +1,33 @@
 package com.roadlogic.models;
 
 import jakarta.persistence.*;
+import java.util.List;
 
 @Entity
 public class Street {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
 
-    private int length; 
+    private double length;
+
+    private String type; 
+
+    private int maxSpeed; 
+
+    private String condition; 
+
+    private String trafficFlow; 
+
+    private boolean isOneWay; 
+
+    @OneToMany(mappedBy = "street", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Intersection> intersections;
+
+    @OneToMany(mappedBy = "street", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Vehicle> vehicles;
 
     // Getters e Setters
     public Long getId() {
@@ -30,11 +46,51 @@ public class Street {
         this.name = name;
     }
 
-    public int getLength() {
+    public double getLength() {
         return length;
     }
 
-    public void setLength(int length) {
+    public void setLength(double length) {
         this.length = length;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+
+    public String getTrafficFlow() {
+        return trafficFlow;
+    }
+
+    public void setTrafficFlow(String trafficFlow) {
+        this.trafficFlow = trafficFlow;
+    }
+
+    public boolean isOneWay() {
+        return isOneWay;
+    }
+
+    public void setOneWay(boolean oneWay) {
+        isOneWay = oneWay;
     }
 }
