c1e990c47fbe08a6d358c6f9a38bb7744c1b16dd
Mateus Farias <mateus.f2001@hotmail.com>
2025-01-17 22:27:17 +0000

    refactor: ajusta RoadLogicApplication para suporte ao StreetController

---
+++ src/main/java/com/roadlogic/RoadLogicApplication.java
@@ -0,0 +1,12 @@
+package com.roadlogic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadLogicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoadLogicApplication.class, args);
+    }
+}
